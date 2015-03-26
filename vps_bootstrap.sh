8c658070777acd0392c8ab4389f791933382707f
Gregory Meno <gmeno@redhat.com>
2015-03-26 20:26:04 +0000

    devmode: add a custom pillar to parameterize salt
    Signed-off-by: Gregory Meno <gmeno@redhat.com>

--- vps_bootstrap.sh
+++ vps_bootstrap.sh
@@ -3,4 +3,4 @@ set -e
 
 curl -o install_salt.sh -L https://bootstrap.saltstack.com
 sudo sh install_salt.sh -P git v2014.7.0
-sudo salt-call --local --file-root=$(pwd)/vagrant/devmode/salt/roots state.highstate
+sudo salt-call --local --file-root=$(pwd)/vagrant/devmode/salt/roots state.highstate pillar='{"username": "centos"}'
